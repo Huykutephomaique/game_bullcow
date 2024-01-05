cba00ab4d5ebfeceaa5fb003caa99bc634a6f0c0
Huykutephomaique <86997615+Huykutephomaique@users.noreply.github.com>
2024-01-05 12:50:42 +0700

    change source

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,50 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    SetUpGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    if (GameOver)
+    {
+        ClearScreen();
+        SetUpGame();
+    }
+    else
+    {
+        ProcessGuess(Input);
+    }
+}
+void UBullCowCartridge::SetUpGame() // Set up
+{
+    GameOver = false;
+    Lives = 4;
+    HiddenWords = TEXT("NGU");
+    PrintLine(TEXT("Now, Let's guess!!!"));
+    PrintLine(TEXT("You have %i lives left"), Lives);
+    PrintLine(TEXT("Hint: The Answer have %i words!"), HiddenWords.Len());
+}
+void UBullCowCartridge::EndPlay() // Game Over
+{
+    GameOver = true;
+    PrintLine(TEXT("Press enter to play again"));
+}
 
+void UBullCowCartridge::ProcessGuess(Fstring Guess) //
+{
+    if (Guess == HiddenWords)
+    {
+        PrintLine(TEXT("Your answer is true: %s"), *HiddenWords);
+        PrintLine(TEXT("U so smart!- Congratulations"));
+        EndPlay();
+    }
+    else
+    {
+        PrintLine(TEXT("Your answer is false: %s"), *Guess);
+        Lives--;
+        PrintLine(TEXT("You have %i lives left"), Lives);
+        if (Lives == 0)
+            EndPlay();
+    }
 }
--- Source/BullCowGame/Console/Cartridge.cpp
+++ Source/BullCowGame/Console/Cartridge.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Cartridge.h"
 #include "Terminal.h"
 #include "GameFramework/Actor.h"
@@ -9,15 +8,15 @@ void UCartridge::BeginPlay()
 {
 	Super::BeginPlay();
 	Terminal = GetOwner()->FindComponentByClass<UTerminal>();
-	checkf(Terminal, TEXT("[%s]: No Terminal found"), *GetName())
+	checkf(Terminal, TEXT("[%s]: No Terminal found"), *GetName());
 }
 
-void UCartridge::PrintLine(const FString& Line) const
+void UCartridge::PrintLine(const FString &Line) const
 {
 	Terminal->PrintLine(Line);
 }
 
-void UCartridge::PrintLine(const TCHAR* Line) const
+void UCartridge::PrintLine(const TCHAR *Line) const
 {
 	Terminal->PrintLine(Line);
 }
